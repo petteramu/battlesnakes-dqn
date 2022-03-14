853bd42d15a717fef04a90e15780e6b3ff31732c
petteramu <petteramu@gmail.com>
2022-03-14 09:20:00 +0100

    Fix the input space having no heads on first turn

--- data_transformation.py
+++ data_transformation.py
@@ -15,14 +15,14 @@ class DataTransformer():
             # [self, other snakes, food]
             input_space.append([[0, 0, 0]] * board.height)
         
-        input_space[self_snake.head.x][self_snake.head.y][0] = -1
         for body_part in self_snake.body:
             input_space[body_part.x][body_part.y][0] = 1
+        input_space[self_snake.head.x][self_snake.head.y][0] = -1
 
         for snake in board.snakes:
-            input_space[snake.head.x][snake.head.y][1] = -1
             for body_part in snake.body:
                 input_space[body_part.x][body_part.y][1] = 1
+            input_space[snake.head.x][snake.head.y][1] = -1
 
         for food in board.food:
             input_space[food.x][food.y][2] = 1 - self_snake.health / 100
