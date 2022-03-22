6b9250daa159c6d45966348f19f629026fe77d6c
tbequinor <tbergs@equinor.com>
2022-03-22 12:43:41 +0100

    Add HatesLosingRewardFunction

--- rewarders.py
+++ rewarders.py
@@ -42,4 +42,24 @@ class SurvivalRewarder(Rewarder):
                 return 1
         
         return 0
+
+
+class HatesLosingRewarder(Rewarder):
+    dead = False
+
+    def determine_reward(self, resulting_state: TurnRequest, snake_name: str):
+        if not resulting_state.board.snakes:
+            return -1
         
+        if len(resulting_state.board.snakes) > 1:
+            snake_names = map(lambda snake: snake.name, resulting_state.board.snakes)
+            if snake_name in snake_names:
+                self.dead = False
+                return 1
+            elif snake_name not in snake_names and not self.dead:
+                self.dead = True
+                return -100
+            else:
+                return 0
+
+        return 0        
